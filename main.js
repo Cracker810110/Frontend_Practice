1d3df5054ee5855ea60918bf33ef4cdc2176a17d
Lowell Cynic <102662862+lowellcynic@users.noreply.github.com>
2024-03-12 00:59:58 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+/* Printing */
+// console.log("Hello World");
+// document.write("Hello World");
+// alert("Hello World");
+// /* Containers & Datatypes */
+
+// /*There are seven types of premative datatypes */
+
+// var a = 1; // Numeric
+// let b = "Hi"; //String
+// const c = 12.003; //Numeric
+// let ab = true; //Boolean
+// let bc = null; //null
+// let ca = undefined; //undefined
+// const abc = BigInt('98742126'); //BigInt
+// let cba = Symbol("Hello"); //Symbol
+
+
+// /* Non-prwmative datatype */
+
+// const abdulMukit = [ {
+//   fullName: "Abdul Mukit Rahman",
+//   age: 17,
+//   class: "10th",
+//   address: "Narayanganj",
+//   martialStatus:null,
+//   jobStatus: undefined
+// },
+// {
+//   state: "Dhaka",
+//   zip_Code: 1400,
+//   roomNum: "8/1"
+// }
+
+// ]
+
+// abdulMukit['fullName']="Cracker912131"
+
+// console.log(abdulMukit['fullName'],typeof('abdulMukit'[jobStatus]))
+
+
+// Arithmetic Oparators
+
+// let x = 4;
+// let y = 8;
+// document.write(x+y);
+// document.write(x-y);
+// document.write(x*y);
+// document.write(x/y);
+// document.write(x%y); //Modulus
+// document.write(x**y); //Exponentiation
+
+// // Unary Operators
+// let z=2;
+// let v=5;
+// console.log("z-- =", z--);
+// console.log(z);
+// console.log("--v =", --v);
+// console.log(v);
+// let t=56;
+// let u=89;
+// console.log("t++ =", t++);
+// console.log(t);
+// console.log("++u", ++u);
+// console.log(u);
+
+// Assingment Operators
+
+as+=94;
+
+
